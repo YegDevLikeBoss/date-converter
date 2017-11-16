942de10b2b9f544053c3fab92572c6c75f39bd6b
YegDevLikeBoss <31534524+YegDevLikeBoss@users.noreply.github.com>
2017-11-16 16:02:32 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,24 +2,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-char c[10], ex[10]="exitprogra";
+char
+c[10],
+cmd[5]="cmnd",
+ex[5]="exit",
+hp[5]="help";
 
 void error(int u);
 void arrio();
 
 int main(int argc, char *argv[])
 {
+    printf("\nHi there ;-)\nThis program is designed to calculate weekday using date\nLet's begin\nPlease read user manual carefully\n\nUSER MANUAL\n");
+    error(6);
+    error(5);
+    error(6);
     if (argc!=1)
-        error(0);
+    {
+        error(6); error(0); error(6);
+    }
     int init=0;
     while(init==0) /*Initialization loop*/
     {
-        int day=0, month=0, year=0, max=0, vis=0, m_code=0, y_code=0, temp=0, aaa=0, y_temp=0, result;
-
-        printf(">>> ");
+        int i, day=0, month=0, year=0, max=0, vis=0, m_code=0, y_code=0, temp=0, y_xx, y_temp=0, result;
+        for (i=0;i<10;i++)
+        {
+            c[i]='\n';
+            if (i<4)
+                cmd[i]='\0';
+        }
+        printf("\t>>> ");
         arrio(); /*Input*/
 
-        if (!strcmp(c, ex)) /*Exit handling*/
+        for (i=0;i<4;i++)
+            cmd[i]=c[i];
+
+        if ((!strcmp(cmd, hp))&&(c[4]=='\0')) /*Help handling*/
+        {
+            error(6); error(5); error(6);
+        }
+
+        if ((!strcmp(cmd, ex))&&(c[4]=='\0')) /*Exit handling*/
         {
             init=1;
             continue;
@@ -30,25 +53,14 @@ int main(int argc, char *argv[])
                 day=((c[0]-'0')*10)+(c[1]-'0');
                 month=((c[3]-'0')*10)+(c[4]-'0');
                 year=((c[6]-'0')*1000)+((c[7]-'0')*100)+((c[8]-'0')*10)+(c[9]-'0');
-                aaa=((c[6]-'0')*10)+(c[7]-'0');
-                switch (aaa)
+                y_xx=((c[6]-'0')*10)+(c[7]-'0');
+                y_xx%=4;
+                switch (y_xx)
                 {
-                    case 15:{ temp=0; break; }
-                    case 16:{ temp=6; break; }
-                    case 17:{ temp=4; break; }
-                    case 18:{ temp=2; break; }
-                    case 19:{ temp=0; break; }
-                    case 20:{ temp=6; break; }
-                    case 21:{ temp=4; break; }
-                    case 22:{ temp=2; break; }
-                    case 23:{ temp=0; break; }
-                    case 24:{ temp=6; break; }
-                    case 25:{ temp=4; break; }
-                    case 26:{ temp=2; break; }
-                    case 27:{ temp=0; break; }
-                    case 28:{ temp=6; break; }
-                    case 29:{ temp=4; break; }
-                    case 30:{ temp=2; break; }
+                    case 0:{ temp=6; break; }
+                    case 1:{ temp=4; break; }
+                    case 2:{ temp=2; break; }
+                    case 3:{ temp=0; break; }
                 }
                 if (year%4==0) /*Leap year checking*/
                 {
@@ -83,35 +95,53 @@ int main(int argc, char *argv[])
                     }
                 } else
                 {
+                    error(6);
                     error(2);
+                    if (!(year>1582||(year==1582&&month>=10&&day>=15)))
+                        error(4);
+                    error(6);
                     continue;
                 }
                 if (day>0&&day<=max)
                 {
-                    if (year>1582&&day<=31&&month<=12&&year<=3099)
+                    if (year>1582||(year==1582&&month>=10&&day>=15))
                     {
                         result=(day+m_code+y_code)%7; /*Main formula*/
                         switch (result) /*Result to string translation*/
                         {
-                            case 0:{ printf("Saturday\n"); break; }
-                            case 1:{ printf("Sunday\n"); break; }
-                            case 2:{ printf("Monday\n"); break; }
-                            case 3:{ printf("Tuesday\n"); break; }
-                            case 4:{ printf("Wednesday\n"); break; }
-                            case 5:{ printf("Thursday\n"); break; }
-                            case 6:{ printf("Friday\n"); break; }
+                            case 0:{ printf("\t it's Saturday\n"); break; }
+                            case 1:{ printf("\t it's Sunday\n"); break; }
+                            case 2:{ printf("\t it's Monday\n"); break; }
+                            case 3:{ printf("\t it's Tuesday\n"); break; }
+                            case 4:{ printf("\t it's Wednesday\n"); break; }
+                            case 5:{ printf("\t it's Thursday\n"); break; }
+                            case 6:{ printf("\t it's Friday\n"); break; }
                             default: break;
                         }
                     } else
-                        error(4);
+                    {
+                        error(6);
+                        error(3);
+                        if (!(year>1582||(year==1582&&month>=10&&day>=15)))
+                            error(4);
+                        error(6);
+                    }
                 } else
-                    error(3);
+                {
+                    error(6);
+                    if (!(month>0&&month<13))
+                        error(2);
+                    error(4);
+                    error(6);
+                }
             } else
             {
-                if (c[0]=='\n')
+                if ((c[0]=='h'&&c[1]=='e'&&c[2]=='l'&&c[3]=='p'&&c[4]=='\0'&&c[5]=='\n')||(c[0]=='\0'&&c[1]=='\n'&&c[2]=='\n'&&c[3]=='\n'&&c[4]=='\n'&&c[5]=='\n'&&c[6]=='\n'&&c[7]=='\n'&&c[8]=='\n'&&c[9]=='\n')) /*Incompatible input error handling*/
                     continue;
                 else
-                    error(1); /*Incompatible input error handling*/
+                {
+                    error(6); error(1); error(6);
+                }
             }
 
             }
@@ -125,11 +155,13 @@ void error(int u)
 {
     switch (u)
     {
-        case 0: { printf("***\nNOTE This programm does not support command line arguments\n***\n"); break; }
-        case 1: { printf("***\nERROR Invalid input format.\nTry: DD.MM.YYYY\n***\n"); break; }
-        case 2: { printf("***\nERROR Invalid month\n***\n"); break; }
-        case 3: { printf("***\nERROR Invalid day\n***\n"); break; }
-        case 4: { printf("***\nNOTE Support only for Georgian calendar from 01.01.1583 to 31.12.3099\n***\n"); break; }
+        case 0: { printf("NOTE\nThis program does not support command line arguments\n"); break; }
+        case 1: { printf("ERROR\nInvalid input format.\nTry: DD.MM.YYYY\n"); break; }
+        case 2: { printf("ERROR\nInvalid month\n"); break; }
+        case 3: { printf("ERROR\nInvalid day\n"); break; }
+        case 4: { printf("NOTE\nSupport only for Georgian calendar from 15.10.1582\n"); break; }
+        case 5: { printf("HELP\n\nInput format:\n DD.MM.YYYY\n\nLegal commands:\n 'exit'\n 'help'\n\n"); error(4); break; }
+        case 6: { printf("________\n"); break; }
     }
 }
 
